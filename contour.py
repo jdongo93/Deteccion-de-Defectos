8ca5b0cfad7b4eaec36da1ce3c2e1ea75aa565ae
jdongo93 <jos_acu93@hotmail.com>
2017-10-31 10:31:55 -0500

    Codigo Base

--- contour.py
+++ contour.py
@@ -5,17 +5,21 @@ img = cv2.resize(original,(300,300))
 cv2.imshow('Original',img)
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#Filtrado de la Imagen 
 blurimg = cv2.GaussianBlur(gris,(5,5),0)
 cv2.imshow('Suavizado',blurimg)
 
-contour = cv2.Canny(blurimg,400,475)
+#Deteccion de Bordes
+contour = cv2.Canny(blurimg,400,475) #Hasta tener una toma estandariza los niveles (400,475) se adaptaran para cada imagen 
+
 
 cv2.imshow('Contornos',contour)
 
+#Cuenta la cantidad de contornos encontrados
 (_, defectos ,_) = cv2.findContours(contour.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 print("Se encontraron {} defectos".format(len(defectos)))
 
+#Dibuja los defectos encontrados en la imagen original 
 cv2.drawContours(img,defectos,-1,(0,255,0),2)
 cv2.imshow("Defectos",img)
 
